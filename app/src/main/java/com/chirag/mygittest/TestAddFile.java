016db796e45c99c285e25a02f5da62e735d47c96
chirag1210 <chirag.chaudhari7@gmil.com>
2018-06-15 20:54:23 +0530

    Modified MainActivity.java added TestAddFile

---
+++ app/src/main/java/com/chirag/mygittest/TestAddFile.java
@@ -0,0 +1,11 @@
+package com.chirag.mygittest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chirag on 06/15/2018.
+ */
+
+public class TestAddFile extends AppCompatActivity{
+
+}
