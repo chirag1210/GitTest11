ea96645ae43b9c584ef4bd4bbebe163fcbac98bb
chirag1210 <chirag.chaudhari7@gmil.com>
2018-06-16 11:13:45 +0530

    modified branch01

--- app/src/main/java/com/chirag/mygittest/MainActivity.java
+++ app/src/main/java/com/chirag/mygittest/MainActivity.java
@@ -12,11 +12,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void TestUpload() {
+
         int i = 1 + 1;
-
-        int ii = 2 + 2;
-
-        int iii = 3 + 3;
 
         String test = "merging branch01";
     }
